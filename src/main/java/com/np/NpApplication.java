c7de621da00f2237c15f1c73ef5eab6e33043893
pavel <ptzekov@gmail.com>
2018-09-11 12:14:30 +0300

    First commit

---
+++ src/main/java/com/np/NpApplication.java
@@ -0,0 +1,13 @@
+package com.np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpApplication.class, args);
+    }
+
+}
