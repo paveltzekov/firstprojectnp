7a5cf998baace11ab89995ac26c59492922d9a32
pavel <ptzekov@gmail.com>
2018-09-11 13:17:50 +0300

    Test

--- src/main/java/com/np/NpApplication.java
+++ src/main/java/com/np/NpApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NpApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NpApplication.class, args);
     }
 
